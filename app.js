0b2ec2810b769f0ebf58b146dfebcda6c10d3221
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-10-19 03:48:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function hideParagraph(){
+
+var Paragraph = document.getElementById("para")
+
+Paragraph.style.display = 'none'
+
+
+}
+
+
+
+
+
+function showParagraph(){
+
+
+
+    var Paragraph = document.getElementById("para")
+
+    Paragraph.style.display = 'block'
+    
+    
+}
